88b5db6967ce6a44f7a18edd05a83648bf5acfad
Martin Carruth <martin.carruth@yahoo.com>
2021-01-06 04:28:28 -0500

    removed old methods that were vulnerable to sql injection and renamed safe methods to standard names.

--- Class2.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SqlHelper
-{
-    public class Class2
-    {
-    }
-}
--- SqlHelper.cs
+++ SqlHelper.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
 
-public class SQL
+public class SqlHelper
 {
     private string Server = "";
     private string Username = "";
@@ -17,38 +15,64 @@ public class SQL
     private string Error = "";
     private SqlConnection sqlConn;
 
-
-    //constructor
-    public SQL()
+    public SqlHelper()
     {
-        Server = "";
-        Username = "";
-        Password = "";
-        Database = "";
-        Command = "";
+        /* *
+        * Instantiate an empty SqlHelper object
+        */
+            
         Trusted_Connection = false;
     }
 
-    public SQL(string server, string db)
+    public SqlHelper(SqlConnection sqlConnection)
     {
+        /* *
+        * Instantiate a a new SqlHelper object with an existing sqlConnection.
+        * This should allow the helper methods to be used with a sqlConnection that is created or provided from outside of this library.
+        * Should not require executing the .Connect() method.
+        */
+        sqlConn = sqlConnection;
+    }
+
+    public SqlHelper(string connectionString)
+    {
+        /* *
+        * Instantiate a SqlHelper object using a premade connection string
+        */
+
+        Trusted_Connection = false;
+        ConnectionString = connectionString;
+    }
+        
+    public SqlHelper(string server, string db)
+    {
+        /* *
+        * Instantiate a SqlHelper object using Windows Trusted Authentication. 
+        */
+
         Server = server;
         Database = db;
         Trusted_Connection = true;
-        Command = "";
     }
-
-    public SQL(string server, string db, string user, string pass)
+        
+    public SqlHelper(string server, string db, string user, string pass)
     {
+        /* *
+        * Instantiate a SqlHelper object with a specified username and password
+        */
+
         Server = server;
         Username = user;
         Password = pass;
         Database = db;
-        Command = "";
         Trusted_Connection = false;
     }
 
     public string GetServer()
     { return Server; }
+
+    public string GetSqlServer()
+    { return sqlConn.ServerVersion; }
 
     public string GetDatabase()
     { return Database; }
@@ -75,7 +99,7 @@ public class SQL
     {
         if (Trusted_Connection)
             ConnectionString = "Server=" + Server + "; Database=" + Database + "; Trusted_Connection=True;";
-        else
+        else if (ConnectionString == "") // SqlHelper objects instantiated with a connection string will skip this and go straight to a connection attempt.
         {
             ConnectionString = "Server=" + Server + "; Database=" + Database + "; User Id=" + Username + "; Password=" + Password + ";";
         }
@@ -87,65 +111,30 @@ public class SQL
         }
         catch (Exception ex)
         {
-            Error = ex.Message;
+            Error = ex.Message; // This can be retrieved using .GetError()
             return false;
         }
     }
 
-    public SqlDataReader Read(string cmd)
-    {
-        SqlCommand CMD = new SqlCommand(cmd, sqlConn);
-        return CMD.ExecuteReader();
-    }
-
-    /* SafeRead() prevents sql injection attacks and is a new version of Read(). Read() will be left in place as legacy, but new projects should use SafeRead()
-     * object[,] inputs - first column is the variable placeholder name, second column is the variable type, third column is the value
-     */
-    public SqlDataReader SafeRead(string cmd, object[,] inputs)
+    /* 
+    * object[,] inputs - first column is the variable placeholder name, second column is the variable type, third column is the value
+    */
+    public SqlDataReader Read(string cmd, object[,] inputs)
     {
         SqlCommand command = new SqlCommand(cmd, sqlConn);
         for (int i = 0; i < inputs.GetLength(0); i++)
         {
-            //command.Parameters.Add((string)inputs[i, 0], inputs[i, 1]);
             command.Parameters.AddWithValue((string)inputs[i, 0], inputs[i, 2]);
         }
         return command.ExecuteReader();
     }
 
-    public List<List<string>> ReadToList(string cmd)
-    {
-        List<List<string>> resultList = new List<List<string>>();
-        List<string> tempList;
-        SqlDataReader reader = Read(cmd);
-
-        var columns = new List<string>();
-
-        for (int i = 0; i < reader.FieldCount; i++)
-        {
-            columns.Add(reader.GetName(i));
-        }
-        resultList.Add(columns);
-        if(reader.HasRows)
-        {
-            while(reader.Read())
-            {
-                tempList = new List<string>();
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    tempList.Add(reader[i].ToString());
-                }
-                resultList.Add(tempList);
-            }
-        }
-        reader.Close();
-        return resultList;
-    }
-    public List<List<string>> ReadToList(string cmd, bool includeHeader)
+    public List<List<string>> ReadToList(string cmd, object[,] inputs, bool includeHeader)
     {
         List<List<string>> resultList = new List<List<string>>();
         List<string> tempList;
 
-        SqlDataReader reader = Read(cmd);
+        SqlDataReader reader = Read(cmd, inputs);
 
         var columns = new List<string>();
 
@@ -173,53 +162,11 @@ public class SQL
         return resultList;
     }
 
-    public List<List<string>> SafeReadToList(string cmd, object[,] inputs, bool includeHeader)
-    {
-        List<List<string>> resultList = new List<List<string>>();
-        List<string> tempList;
-
-        SqlDataReader reader = SafeRead(cmd,inputs);
-
-        var columns = new List<string>();
-
-        if (includeHeader)
-        {
-            for (int i = 0; i < reader.FieldCount; i++)
-            {
-                columns.Add(reader.GetName(i));
-            }
-            resultList.Add(columns);
-        }
-        if (reader.HasRows)
-        {
-            while (reader.Read())
-            {
-                tempList = new List<string>();
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    tempList.Add(reader[i].ToString());
-                }
-                resultList.Add(tempList);
-            }
-        }
-        reader.Close();
-        return resultList;
-    }
-
-    public int Write(string cmd)
-    {
-        SqlCommand CMD = new SqlCommand(cmd, sqlConn);
-        return CMD.ExecuteNonQuery();
-    }
-
-    public int SafeWrite(string cmd, object[,] inputs)
+    public int Write(string cmd, object[,] inputs)
     {
         SqlCommand CMD = new SqlCommand(cmd, sqlConn);
         for (int i = 0; i < inputs.GetLength(0); i++)
         {
-            //command.Parameters.Add((string)inputs[i, 0], inputs[i, 1]);
-            //CMD.Parameters.AddWithValue((string)inputs[i, 0], inputs[i, 2]);
-
             SqlParameter param = new SqlParameter();
             param.ParameterName = (string)inputs[i, 0];
             param.SqlDbType = (SqlDbType)inputs[i, 1];
